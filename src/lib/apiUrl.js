46be2a1c13c4c8526e458c552ed094c4b1836cbf
Connor Richard Davis <st10068305@vcconnect.edu.za>
2024-06-04 15:55:58 +0200

    Use prod api url

--- src/lib/apiUrl.js
+++ src/lib/apiUrl.js
@@ -1,2 +1,2 @@
-// export default "https://autoreports-api.corecapital.co.za";
-export default "http://localhost:4000";
+export default "https://autoreports-api.corecapital.co.za";
+// export default "http://localhost:4000";
